604a815792bc97cc300882e866598f28b12bf6ea
Lanre Adelowo <me@lanreadelowo.com>
2017-03-06 00:39:51 +0100

    Next generation Pdf designer

---
+++ algorithms/designer_pdf_viewer.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+	"fmt"
+)
+
+const WIDTH = 1
+
+func main() {
+
+	var reader = bufio.NewReader(os.Stdin)
+
+	h, _ := reader.ReadString('\n')
+
+	wordHeights := make(map[int]int, 26)
+
+	for k, v := range strings.Split(strings.TrimSpace(h), " ") {
+		i, _ := strconv.Atoi(v)
+		wordHeights[k] = i
+	}
+
+	w, _ := reader.ReadString('\n')
+
+	var highest int
+
+	for _, v := range strings.TrimSpace(w) {
+		temp := wordHeights[int(v - rune('a'))]
+
+		if temp > highest{
+			highest = temp
+		}
+	}
+
+	fmt.Println((len(strings.Split(strings.TrimSpace(w), "")) * WIDTH) * highest)
+}
